b038f4087e3202ec079f302e057d798879855712
Antoine Caputo <antoine.caputo@lenstra.fr>
2024-01-19 23:52:35 +0100

    feat: improve playlist searching

--- static/js/index.js
+++ static/js/index.js
@@ -1,24 +1,55 @@
-let selectedPlaylist = "movies";
+let selectedPlaylist = "";
+
+function clearSearchInput() {
+    $("#query").val("");
+    $("#search-results").empty();
+}
+
+function selectPlaylist(selector) {
+    const elementId = selector.currentTarget.id;
+
+    console.log('selector:', selector.currentTarget);
+
+    console.log('elementId:', elementId);
+
+    // set class to bg-gray-200 for all button from #playlist-selector
+    $("#playlist-selector button").removeClass("bg-blue-200");
+
+    // set class to bg-blue-200 for selected element
+    $(`#playlist-selector #${elementId}`).addClass("bg-blue-200");
+
+    selectedPlaylist = elementId.replace("playlist-", "")
+        .replace("all", "")
+        .toUpperCase();
+
+    search();
+}
 
 function search() {
-    const limit = Number($("#result-count").val());
+    const limit = 100; //| Number($("#result-count").val());
     console.log('limit', limit);
-
-    const filter = $("#filter").val();
-    console.log('filter', filter.toString());
 
     const query = $("#query").val();
     console.log('query', query.toString());
 
-    const url = `/search?limit=${limit}&filter=${filter}&query=${query}`;
 
-    console.log(url);
-
-    if (query.length >= 3) {
-        $.get(url, function(data) {
-            $("#search-results").html(data);
-        });
-    } else {
+    if(query.length < 3) {
         $("#search-results").empty();
+        return;
     }
+
+    const url = `/search?limit=${limit}&playlist=${selectedPlaylist}&query=${query}`;
+    console.log('url:', url);
+
+    $.get(url, function(data) {
+        $("#search-results").html(data);
+    });
+}
+
+function displayResumes() {
+    const url = "/resumes";
+
+    $.get(url, function(data) {
+        $("#search-results").html(data);
+    });
 }
--- web/search.go
+++ web/search.go
@@ -1,7 +1,6 @@
 package web
 
 import (
-	"fmt"
 	"iptvstudio/library"
 	"net/http"
 	"strconv"
@@ -13,14 +12,14 @@ func searchHandler(w http.ResponseWriter, r *http.Request, lib *library.Library)
 		limit = 10
 	}
 
-	filter := r.URL.Query().Get("filter")
+	playlist := r.URL.Query().Get("playlist")
 
 	query := r.URL.Query().Get("query")
 
 	var result []*library.Channel
 
-	if filter != "" {
-		result = lib.Search(library.Playlist(filter), query)
+	if playlist != "" {
+		result = lib.Search(library.Playlist(playlist), query)
 	} else {
 		result = lib.MasterSearch(query)
 	}
@@ -42,12 +41,16 @@ func searchHandler(w http.ResponseWriter, r *http.Request, lib *library.Library)
 
 func searchResultHTML(channel *library.Channel) string {
 
-	playerLink := fmt.Sprintf("rtsp://%s", channel.Url)
+	//playerLink := fmt.Sprintf("rtsp://%s", channel.Url)
+
+	// openVLC := `window.open("` + player.PlayCommand(channel.Url) + `")`
+
+	download := `window.open("` + channel.Url + `")`
 
 	actions := `
 		<button
 			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-			onclick="window.location.href='` + channel.Url + `'">
+			onclick="` + "" + `">
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 					<path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
 				</svg>
@@ -55,7 +58,7 @@ func searchResultHTML(channel *library.Channel) string {
 
 		<button
 			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-			onclick="window.location.href='` + playerLink + `'">
+			onclick="` + download + `">
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 					<path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
 				</svg>
@@ -64,7 +67,7 @@ func searchResultHTML(channel *library.Channel) string {
 	return `
 		<div class="channel mb-4">
     		<div class="flex flex-col items-center justify-center">
-				<div class="channel__logo cursor-pointer" onclick="window.location.href='` + playerLink + `'">
+				<div class="channel__logo">
 					<img class="h-32" src="` + string(channel.Logo) + `" alt="` + string(channel.Name) + `">
 				</div>
 
