077b039fd2a9ee23d08f7c9419d1aa69410f5fe4
Antoine Caputo <antoine.caputo@lenstra.fr>
2024-01-19 15:07:39 +0100

    style: result web component

--- web/search.go
+++ web/search.go
@@ -25,7 +25,8 @@ func searchHandler(w http.ResponseWriter, r *http.Request, lib *library.Library)
 		result = lib.MasterSearch(query)
 	}
 
-	resultHTML := "<div class=\"search-result\">"
+	var resultHTML string
+
 	for i, channel := range result {
 		if i >= limit {
 			break
@@ -33,7 +34,6 @@ func searchHandler(w http.ResponseWriter, r *http.Request, lib *library.Library)
 
 		resultHTML += searchResultHTML(channel)
 	}
-	resultHTML += "</div>"
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
@@ -44,17 +44,35 @@ func searchResultHTML(channel *library.Channel) string {
 
 	playerLink := fmt.Sprintf("rtsp://%s", channel.Url)
 
+	actions := `
+		<button
+			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+			onclick="window.location.href='` + channel.Url + `'">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
+				</svg>
+		</button>
+
+		<button
+			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+			onclick="window.location.href='` + playerLink + `'">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+				</svg>
+		</button>`
+
 	return `
-		<div class="channel">
-			<div style="flex-direction: column; display: flex; justify-content: center; align-items: center;">
-				<div class="channel__logo" onclick="window.location.href='` + playerLink + `'">
-					<img src="` + string(channel.Logo) + `" alt="` + string(channel.Name) + `">
+		<div class="channel mb-4">
+    		<div class="flex flex-col items-center justify-center">
+				<div class="channel__logo cursor-pointer" onclick="window.location.href='` + playerLink + `'">
+					<img class="h-32" src="` + string(channel.Logo) + `" alt="` + string(channel.Name) + `">
 				</div>
+
 				<div class="channel__name">` + string(channel.Name) + `</div>
+
 				<div class="channel__group">` + string(channel.Group) + `</div>
-				<div class="channel__download">
-					<li><a href="` + string(channel.Logo) + `" download>Download</a></li>
-				</div>
+
+				<div class="mb-4 flex items-center space-x-2">` + actions + `</div>			
 			</div>
 		</div>
 	`
