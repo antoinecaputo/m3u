73de91e19668f19ebbbb1036ddaf781cdbf5200d
Antoine Caputo <antoine.caputo@lenstra.fr>
2024-01-20 23:41:46 +0100

    feat: load with toast

--- static/js/index.js
+++ static/js/index.js
@@ -46,10 +46,81 @@ function search() {
     });
 }
 
+let loadLock = false;
+function load() {
+    if(loadLock) {
+        showToast("Please wait for the current load to finish", false);
+        return;
+    }
+
+    $.get("/load", function(data) {
+        showToast(data, true);
+    }).fail(function(e) {
+        showToast(e.responseText, false);
+    }).always(function() {
+        loadLock = false;
+    });
+}
+
 function displayResumes() {
     const url = "/resumes";
 
     $.get(url, function(data) {
         $("#search-results").html(data);
     });
+}
+
+
+function showToast(message, success = true) {
+    const toastElement = $("#toast-container");
+
+    // A message is already displayed
+    if (!toastElement.hasClass("hidden")) {
+        console.log('toast is not hidden');
+
+        // opacity-100 is always treated as priority on opacity-0
+        // so add it first for animation in
+        toastElement.addClass("opacity-0");
+        toastElement.removeClass("opacity-100");
+
+        setTimeout(function() {
+            toastElement.addClass("hidden");
+            showToast(message, success);
+        }, 300);
+
+        return;
+    }
+
+    const successElement = $("#toast-container #toast-success");
+    const errorElement = $("#toast-container #toast-error");
+
+    if(success) {
+        successElement.removeClass("hidden");
+        errorElement.addClass("hidden");
+    } else {
+        successElement.addClass("hidden");
+        errorElement.removeClass("hidden");
+    }
+
+    const messageElement = $("#toast-container #toast-message");
+    messageElement.text(message);
+
+    toastElement.removeClass("hidden");
+
+    // opacity-100 is always treated as priority on opacity-0
+    // so add it first for animation in
+    toastElement.addClass("opacity-100");
+    toastElement.removeClass("opacity-0");
+
+    setTimeout(function() {
+        // opacity-100 is always treated as priority on opacity-0
+        // so remove it last for animation out
+        toastElement.addClass("opacity-0");
+        toastElement.removeClass("opacity-100");
+
+        setTimeout(function() {
+            toastElement.addClass("hidden");
+        }, 500);
+
+    }, 5000);
 }
